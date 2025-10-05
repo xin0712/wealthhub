c64cbe27db990d3ad71cc97189aca1f5f17d4321
xin0712 <gpxinxin@gmail.com>
2025-10-05 23:08:40 +0800

    build: relax broken links to warn for Vercel

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,7 +25,8 @@ const config: Config = {
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
